1fc46910c46274dbf275a0138713102bd6d6d170
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-09-07 21:52:51 +0000

    Updated | settings.py

--- GeekTide_Django/settings.py
+++ GeekTide_Django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r!_05^rsm0)%_mt$6i33+9cueenfws$2jp7+siqdi=*r)lbyad'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
