2477afd6d67b2fed0957e704dc2f667b751d9975
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-09-16 22:59:54 +0000

    Added | Bunch of templates

--- GeekTide/urls.py
+++ GeekTide/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),  # Главная страница
+    path('load-posts/', views.load_posts, name='load_posts'),  # Новый URL для подгрузки постов
+    path('profile/<str:nickname>/', views.profile, name='profile'),
 ]
--- GeekTide/views.py
+++ GeekTide/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post
+from .models import Post, Profile
+from django.http import JsonResponse
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.all().order_by('-created_at')  # Получаем все посты, отсортированные по дате
-    return render(request, 'index.html', {'posts': posts})  # Передаем посты в шаблон
+    # Это представление для начальной загрузки страницы
+    posts_list = Post.objects.all()
+    paginator = Paginator(posts_list, 10)  # Показываем по 2 поста на странице
+
+    page_number = request.GET.get('page') or 1
+    posts = paginator.get_page(page_number)
+
+    return render(request, 'index.html', {'posts': posts})
+
+def load_posts(request):
+    # Это представление для AJAX-запросов (динамическая подгрузка постов)
+    page_number = request.GET.get('page')  # Получаем номер страницы из запроса
+    posts_list = Post.objects.all()
+    paginator = Paginator(posts_list, 10)
+
+    try:
+        posts = paginator.get_page(page_number)
+    except:
+        posts = []
+
+    # Формируем данные для ответа в формате JSON
+    posts_data = []
+    for post in posts:
+        posts_data.append({
+            'title': post.title,
+            'description': post.description,
+            'image_url': post.image.url if post.image else None,
+            'owner': post.owner.username,
+            'created_at': post.created_at.strftime('%Y-%m-%d'),
+            'tags': post.get_tags_list(),
+        })
+
+    return JsonResponse({
+        'posts': posts_data,
+        'has_next': posts.has_next(),
+    })
+
+def profile(request, nickname):
+    user_profile = get_object_or_404(Profile, nickname=nickname)
+    return render(request, 'profile.html', {'profile': user_profile})
